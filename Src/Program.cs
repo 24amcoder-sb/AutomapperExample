502dbcf2fe7bc4374ec3004a87793e049ca04c06
S Lokesh Swamy <lokesh.selvaraj@hotmail.com>
2019-09-11 19:57:50 +0530

    Automapper library & basic setup code added

---
+++ Src/Program.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using static System.Console;
+
+namespace AutomapperConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<Source, Destination>());
+
+            var source = new Source() { Value = 101 };
+            var destination = Mapper.Map<Destination>(source);
+        }
+    }
+
+    public class Source
+    {
+        public int Value { get; set; }
+    }
+
+    public class Destination
+    {
+        public int Value { get; set; }
+    }
+}
