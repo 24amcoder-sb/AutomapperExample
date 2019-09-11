917f54b69b76d61b22ad55b6b7b92a3cb47d9962
S Lokesh Swamy <lokesh.selvaraj@hotmail.com>
2019-09-11 23:43:57 +0530

    before and after map example added

---
+++ Src/AutomapperConsole/08_before_after_map/Sample_08.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using static System.Console;
+
+namespace AutomapperConsole
+{
+    class Sample_08
+    {
+        static void Main()
+        {
+            //Without CallMeJackJones
+
+            //Mapper.Initialize(cfg => cfg.CreateMap<Person, PersonDto>()
+            //                            .BeforeMap((src, dest) => src.Age = src.Age + 10 )
+            //                            .AfterMap((src, dest) => dest.Name =  $"Jack {dest.Name}"));
+
+
+
+
+            // With CallMeJackJones
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Person, PersonDto>()
+                                        .BeforeMap((src, dest) => src.Age = src.Age + 10)
+                                        .AfterMap<CallMeJackJones>());
+
+            var destination = Mapper.Map<PersonDto>(new Person { Name = "Jones" });
+
+            WriteLine(destination.Name == "Jack Jones");
+            WriteLine(destination.Age == 10);
+
+            ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class PersonDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class CallMeJackJones : IMappingAction<Person, PersonDto>
+    {
+        public void Process(Person source, PersonDto destination)
+        {
+            destination.Name = $"Jack {destination.Name}";
+        }
+    }
+}
